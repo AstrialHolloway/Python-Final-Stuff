cd1aa16a55edf36089021fb199a210f7fc98ef51
Astrial <mlsgamer09@gmail.com>
2026-04-07 11:45:24 -0500

    Add initial Snake game implementation
    Implement basic structure for Snake game with restart functionality.

---
+++ snake.py
@@ -0,0 +1,18 @@
+# Since the Tetris is almost finished, my teacher might not let me use it for the final :<
+# There is a chance I can still use it, but I will be making Snake as a backup :3
+
+from cmu_graphics import *
+import os
+import sys
+import random
+import time
+
+#Restarts program so I dont have to mess with closing and opening the exe thing again
+def restart_program():
+    python = sys.executable
+    os.execv(python, [python] + sys.argv)
+
+def onKeyPress(key):
+  if (key=='p'):
+    restart_program()
+cmu_graphics.run()
